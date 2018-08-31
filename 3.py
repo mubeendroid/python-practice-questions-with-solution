1edfc2a1acbf54019496ac25822ac5d30d7b3807
Mubeen Ul Alam <mubeendroid@gmail.com>
2018-08-31 12:30:44 +0600

    practice python

---
+++ 3.py
@@ -0,0 +1,10 @@
+'''
+With a given integral number n, write a program to generate a dictionary that
+contains (i, i*i) such that is an integral number between 1 and n (both included).
+and then the program should print the dictionary.
+'''
+n=int(input())
+d=dict()
+for i in range(1,n+1):
+    d[i]=i*i
+print(d)
