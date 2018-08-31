1cedf2c29be8d483bbf55c2fe29bc1b841082371
Mubeen Ul Alam <mubeendroid@gmail.com>
2018-08-31 12:33:11 +0600

    practice python

---
+++ 4.py
@@ -0,0 +1,9 @@
+'''
+Write a program which accepts a sequence of comma-separated numbers
+from console and generate a list and a tuple which contains every number.
+'''
+x=input()
+l=x.split(',')
+t=tuple(l)
+print(l)
+print(t)
