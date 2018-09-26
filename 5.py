62418e30f9972378af6c7bbe9b9469bc3ccd3a8e
Mubeen Ul Alam <mubeendroid@gmail.com>
2018-09-26 19:21:17 +0600

    practice python

--- 5.py
+++ 5.py
@@ -4,6 +4,7 @@ getString: to get a string from console input
 printString: to print the string in upper case.
 Also please include simple test function to test the class methods.
 '''
+
 class printClass():
     def __init__(self):
         self.s=""
